81d892edcf82dc409d13012a6d48e6c44e2769c0
cyborg2090 <petermuiruri1056@gmail.com>
2020-05-23 21:06:56 +0300

    working_on-it

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const route = express.Router();
+const http = require('http');
+
   //render home page
 route.get('/',(req,res)=>{
     res.render('index');
@@ -14,4 +16,37 @@ route.get('/',(req,res)=>{
   route.get('/contact',(req,res)=>{
     res.render('contact');
   });
+  route.post('/recipe',(req,res)=>{
+    
+    cook = req.body;
+    var id = '5012aa7f';
+    var app_key = '55d4e3a4a5a303622ca981233920357d';
+    var options = {
+      "method": "GET",
+      "hostname": `api.edamam.com/search?q=chichen`,
+      "port": null,
+      "headers":{
+        "app_id":id,
+        "app_key":app_key,
+        "useQueryString": true
+      }
+    };
+
+      //make request
+      var req = http.request(options,function(res){
+        var chunks = [];
+        res.on('data',function(chunk){
+          chunks.push(chunk);
+        });
+        res.on('end',function(){
+          var body = Buffer.concat(chunks);
+          console.log(body.toJSON());
+        });
+      });
+      
+      req.end();
+    
+    
+  });
+  
 module.exports = route;
