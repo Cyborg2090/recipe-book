02bd15e2a4320ef594eba6f3dfa8933343621774
cyborg2090 <petermuiruri1056@gmail.com>
2020-06-23 20:35:23 +0300

    leaving

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const route = express.Router();
-const http = require('http');
-
+const http = require('https');
   //render home page
 route.get('/',(req,res)=>{
     res.render('index');
@@ -16,15 +15,16 @@ route.get('/',(req,res)=>{
   route.get('/contact',(req,res)=>{
     res.render('contact');
   });
-  route.post('/recipe',(req,res)=>{
+  route.get('/recipe',(req,res)=>{
     
-    cook = req.body;
     var id = '5012aa7f';
     var app_key = '55d4e3a4a5a303622ca981233920357d';
+  
     var options = {
       "method": "GET",
-      "hostname": `api.edamam.com/search?q=chichen`,
-      "port": null,
+      "Content-Encoding": 'application/json',
+      "hostname":encodedURI(`api.edamam.com/search?q= ${req.body}`),
+      "port": 80,
       "headers":{
         "app_id":id,
         "app_key":app_key,
@@ -40,12 +40,18 @@ route.get('/',(req,res)=>{
         });
         res.on('end',function(){
           var body = Buffer.concat(chunks);
-          console.log(body.toJSON());
+          var json = JSON.stringify(body);
+          console.log(json.toString(json));
+          
+          
+         
         });
+        
       });
       
       req.end();
-    
+      
+      
     
   });
   
